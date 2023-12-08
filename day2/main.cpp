a551d0b6dc83da724f95e7450da96b8d6fb4ada2
Bat <baptiste.hommeril@gmail.com>
2023-12-08 15:29:34 +0100

    d2 ok

--- day2/main.cpp
+++ day2/main.cpp
@@ -2,27 +2,89 @@
 #include <string>
 #include <fstream>
 
-bool	isPossible(std::string str) {
+int	CubeTot(std::string& str) {
 	
-//	int	red = 12;
-//	int	green = 13;
-//	int	blue = 14;
+	int	red = 0;
+	int	green = 0;
+	int	blue = 0;
 	
 	size_t start = str.find(":");
 	std::string temp = str.substr(start);
 	size_t end  = 0;;
 	while (end != std::string::npos) {
 		int red_temp = 0;
-	//	int	blue_temp = 0;
-	//	int	green_temp = 0;
+		int	blue_temp = 0;
+		int	green_temp = 0;
 		end = temp.find(";");
 		if (temp.find("red") < end) {
 			red_temp = stoi(temp.substr(temp.find("red") - 3));
-			std::cout << "RED = " << red_temp << " ";
+			if (red_temp > red)
+				red = red_temp;
+		}	
+		if (temp.find("green") < end) {
+			green_temp = stoi(temp.substr(temp.find("green") - 3));
+			if (green_temp > green)
+				green = green_temp;
+		}	
+		if (temp.find("blue") < end) {
+			blue_temp = stoi(temp.substr(temp.find("blue") - 3));
+			if (blue_temp > blue)
+				blue = blue_temp;
 		}	
 		start = end + 1;
-		temp = str.substr(start);
-		std::cout << "[" << temp << "]" << std::endl;
+		temp = temp.substr(start);
+	}	
+	return (green * blue * red);
+}		
+
+
+int	main()
+{
+	std::ifstream ifs;
+
+	ifs.open("input", std::ifstream::in);
+	std::string str;
+	int	total = 0;
+	while (getline(ifs, str))
+	{
+		total += CubeTot(str);
+	}
+	std::cout << "FINAL ANSWER " << total << std::endl;
+}
+/* PART I
+bool	isPossible(std::string str) {
+	
+	int	red = 12;
+	int	green = 13;
+	int	blue = 14;
+	
+	size_t start = str.find(":");
+	std::string temp = str.substr(start);
+	size_t end  = 0;;
+	while (end != std::string::npos) {
+		int red_temp = 0;
+		int	blue_temp = 0;
+		int	green_temp = 0;
+		end = temp.find(";");
+		if (temp.find("red") < end) {
+			red_temp = stoi(temp.substr(temp.find("red") - 3));
+		//	std::cout << "RED = " << red_temp << " ";
+			if (red_temp > red)
+				return false;
+		}	
+		if (temp.find("green") < end) {
+			green_temp = stoi(temp.substr(temp.find("green") - 3));
+			if (green_temp > green)
+				return false;
+		}	
+		if (temp.find("blue") < end) {
+			blue_temp = stoi(temp.substr(temp.find("blue") - 3));
+			if (blue_temp > blue)
+				return false;
+		}	
+		start = end + 1;
+		temp = temp.substr(start);
+	//	std::cout << "[" << temp << "]" << std::endl;
 	}	
 	return true;
 }		
@@ -43,4 +105,4 @@ int	main()
 		Game++;
 	}
 	std::cout << "FINAL ANSWER" << total << std::endl;
-}
+}*/
